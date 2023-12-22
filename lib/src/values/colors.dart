6de04a602a119ff59cb6b862d91551937353e499
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-12-22 14:55:31 +0700

    refactor directory project using repository design pattern

---
+++ lib/src/values/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color bluePrimary = const Color(0xff357AD4);
+  static Color white = const Color(0xFFF3F7F8);
+  static Color grey = const Color(0xFF8E8E8E);
+}
